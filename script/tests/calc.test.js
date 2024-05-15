7d2f527fec0e9368df81dd8955a64adb7a742851
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-05-15 07:11:21 +0000

    Make first jest test

---
+++ script/tests/calc.test.js
@@ -0,0 +1,15 @@
+const addition = require("../calc");
+
+describe("Calculation", () => {
+    describe("Addition function", () => {
+        test("should retrun 42 for 20 + 22", () =>{
+            expect(addition(20,22)).toBe(42);
+        })
+    });
+    describe("Subtration function", () => {
+        test 
+    });
+    describe("Multiply function", () => {
+        test 
+    });
+})
