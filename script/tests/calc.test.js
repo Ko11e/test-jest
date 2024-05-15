71b535cb51f119f1ffcc644ecefe963fa27a60a7
Emmelie Kolmodin <e.kolmodin@hotmail.com>
2024-05-15 07:19:36 +0000

    Add on other test

--- script/tests/calc.test.js
+++ script/tests/calc.test.js
@@ -1,10 +1,18 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const addition = require("../calc");
 
 describe("Calculation", () => {
     describe("Addition function", () => {
-        test("should retrun 42 for 20 + 22", () =>{
+        test("should retrun 42 for 20 + 22", () => {
             expect(addition(20,22)).toBe(42);
         })
+        test("should retrun 73 for 42 + 31", () => {
+            expect(addition(42,31)).toBe(73);
+        })
+
     });
     describe("Subtration function", () => {
         test 
